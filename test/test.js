8e6cf4c1f487ef528bc7899b13e23910a8065118
Andrey Sitnik <andrey@sitnik.ru>
2014-11-12 18:49:46 +0300

    Use Chai.js instead of Should.js

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
-require('should');
+var expect = require('chai').expect;
 
 describe('postcss-loader', function() {
 
     it('processes CSS with default plugins', function () {
         var css = require('!raw-loader!../!./cases/style.css');
-        css.should.containEql('a { color: red }');
+        expect(css).to.eql('a { color: red }\n');
     });
 
     it('processes CSS with custom plugins', function () {
         var css = require('!raw-loader!../?pack=blues!./cases/style.css');
-        css.should.containEql('a { color: blue }');
+        expect(css).to.eql('a { color: blue }\n');
     });
 
     it('processes CSS in safe mode', function () {
         var css = require('!raw-loader!../?safe=1!./cases/broken.css');
-        css.should.containEql('a { color: red\n}');
+        expect(css).to.eql('a { color: red\n}');
     });
 
 });
