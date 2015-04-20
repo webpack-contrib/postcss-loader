346b58a0e4693f9bb25ca4271e7518a3a027983d
Richard Willis <willis.rh@gmail.com>
2015-04-20 22:14:58 +0100

    Use previous source map and don't inline the source maps.
    This fixes source mapping when used with sass-loader.
    Also, we don't want to map to the postcss generated source, we want to map to the pre-postcss source.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var loaderUtils = require('loader-utils');
 var postcss     = require('postcss');
 
-module.exports = function (source) {
+module.exports = function (source, map) {
     if ( this.cacheable ) this.cacheable();
 
     var file    = loaderUtils.getRemainingRequest(this);
     var params  = loaderUtils.parseQuery(this.query);
 
-    var opts = { from: file, to: file };
+    var opts = { from: file, to: file, map: { prev: map, inline: false } };
     if ( params.safe ) opts.safe = true;
 
     var processors = this.options.postcss;
