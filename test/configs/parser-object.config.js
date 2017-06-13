4da7688dbfeb7a8ae2598fa44769e11035a6bce1
Mathias Hoffmann <hoffmann.mathias@gmail.com>
2017-06-14 01:21:29 +0200

    Allow to pass an object to "Parser" option
    Before, we had to pass a string to "Parser" option.

---
+++ test/configs/parser-object.config.js
@@ -0,0 +1,8 @@
+const sugarss = require('sugarss')
+
+module.exports = {
+  file: 'parser-object',
+  options: {
+    parser: sugarss
+  }
+}
--- test/configs/parser-object.js
+++ test/configs/parser-object.js
@@ -1,4 +1,4 @@
-test('Parser', () => {
+test('Parser as an object', () => {
   const css = require('../fixtures/style.sss')
   expect(css).toEqual('a {\n  color: black\n}\n')
 })
