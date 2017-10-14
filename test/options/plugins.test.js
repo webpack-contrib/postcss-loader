df010a7b13acae2e9abd18690055c713cec4191a
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-10-14 17:50:57 +0200

    fix(lib/options): handle `{Object}` return (`options.plugins`) (#301)

--- test/options/plugins.test.js
+++ test/options/plugins.test.js
@@ -22,12 +22,48 @@ describe('Options', () => {
       })
   })
 
-  test('Plugins - {Function}', () => {
+  test('Plugins - {Object}', () => {
+    const config = {
+      loader: {
+        options: {
+          ident: 'postcss',
+          plugins: require('../fixtures/config/plugin')
+        }
+      }
+    }
+
+    return webpack('css/index.js', config).then((stats) => {
+        const src = loader(stats).src
+
+        expect(src).toEqual("module.exports = \"a { color: rgba(255, 0, 0, 1.0) }\\n\"")
+        expect(src).toMatchSnapshot()
+      })
+  })
+
+  test('Plugins - {Function} - {Array}', () => {
     const config = {
       loader: {
         options: {
           ident: 'postcss',
           plugins: () => [ require('../fixtures/config/plugin')() ]
+        }
+      }
+    }
+
+    return webpack('css/index.js', config).then((stats) => {
+        const src = loader(stats).src
+
+        expect(src).toEqual("module.exports = \"a { color: rgba(255, 0, 0, 1.0) }\\n\"")
+        expect(src).toMatchSnapshot()
+      })
+  })
+
+  test('Plugins - {Function} - {Object}', () => {
+    const config = {
+      loader: {
+        options: {
+          ident: 'postcss',
+          plugins: () => require('../fixtures/config/plugin')()
         }
       }
     }
