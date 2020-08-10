ee7be162069158c59efef97b9f98b3a4b8823586
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-08-10 13:49:32 +0300

    fix: exec option for @webpack5

--- test/options/exec.test.js
+++ test/options/exec.test.js
@@ -9,35 +9,34 @@ import {
 } from '../helpers/index';
 
 describe('Options Exec', () => {
-  // Todo loaderContext.exec is deprecated and removed in webpack 5
-  // it('should work Exec - {Boolean}', async () => {
-  //   const compiler = getCompiler(
-  //     './jss/exec/index.js',
-  //     {},
-  //     {
-  //       module: {
-  //         rules: [
-  //           {
-  //             test: /style\.(exec\.js|js)$/i,
-  //             use: [
-  //               {
-  //                 loader: path.resolve(__dirname, '../../src'),
-  //                 options: { exec: true },
-  //               },
-  //             ],
-  //           },
-  //         ],
-  //       },
-  //     }
-  //   );
-  //   const stats = await compile(compiler);
-  //
-  //   const codeFromBundle = getCodeFromBundle('style.exec.js', stats);
-  //
-  //   expect(codeFromBundle.css).toMatchSnapshot('css');
-  //   expect(getWarnings(stats)).toMatchSnapshot('warnings');
-  //   expect(getErrors(stats)).toMatchSnapshot('errors');
-  // });
+  it('should work Exec - {Boolean}', async () => {
+    const compiler = getCompiler(
+      './jss/exec/index.js',
+      {},
+      {
+        module: {
+          rules: [
+            {
+              test: /style\.(exec\.js|js)$/i,
+              use: [
+                {
+                  loader: path.resolve(__dirname, '../../src'),
+                  options: { exec: true },
+                },
+              ],
+            },
+          ],
+        },
+      }
+    );
+    const stats = await compile(compiler);
+
+    const codeFromBundle = getCodeFromBundle('style.exec.js', stats);
+
+    expect(codeFromBundle.css).toMatchSnapshot('css');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
+  });
 
   it('should work JSS - {String}', async () => {
     const compiler = getCompiler(
