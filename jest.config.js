eb9937de491e34e7d71b57318d2e2e9f650c1aa2
alexander-akait <sheo13666q@gmail.com>
2025-07-28 16:34:55 +0300

    test: update

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  testTimeout: 10000,
   transform: {
     "\\.[mc]?tsx?$": ["ts-jest", { rootDir: ".", esModuleInterop: true }],
     "\\.[mc]?jsx?$": "babel-jest",
